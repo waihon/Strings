cae8ff75e3642cee8511daa73c23c98c055acce9
Waihon Yew <yewwaihon@gmail.com>
2021-05-29 17:27:11 +0800

    Initial Commit

---
+++ Strings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Strings
+//
+//  Created by Waihon Yew on 29/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
